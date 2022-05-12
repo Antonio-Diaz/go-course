f1890205eec527f38f98a1f77175b37095b5d9d1
Antonio Diaz <j.antonio.udiaz@gmail.com>
2022-05-12 13:29:17 -0500

    feat(demo): basic use if-else

--- demo/if-else/if-else.go
+++ demo/if-else/if-else.go
@@ -8,5 +8,19 @@ func average(a, b, c int) float32 {
 }
 
 func main() {
+	quiz1, quiz2, quiz3 := 1, 2, 3
 
+	if quiz1 > quiz2 {
+		fmt.Println("quiz1 is greater than quiz2")
+	} else if quiz1 < quiz2 {
+		fmt.Println("quiz1 is less than quiz2")
+	} else {
+		fmt.Println("quiz1 is equal to quiz2")
+	}
+
+	if average(quiz1, quiz2, quiz3) > 5 {
+		fmt.Println("Average is greater than 5")
+	} else {
+		fmt.Println("Average is less than or equal to 5")
+	}
 }
