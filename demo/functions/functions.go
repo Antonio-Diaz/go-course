51f53676e229ab2d553aad3396401334c2da5e66
Antonio Diaz <j.antonio.udiaz@gmail.com>
2022-05-12 12:36:49 -0500

    feat(demo): basic use functions

--- demo/functions/functions.go
+++ demo/functions/functions.go
@@ -2,6 +2,27 @@ package main
 
 import "fmt"
 
-func main() {
+func double(x int) int {
+	return x * x
+}
 
+func add(lhs, rhs int) int {
+	return lhs + rhs
+}
+
+func greet() {
+	fmt.Println("Greets from functions.go")
+}
+
+func main() {
+	greet()
+
+	dozen := double(12)
+	fmt.Println("12 squared is", dozen)
+
+	sum := add(dozen, 1)
+	fmt.Println("12 squared + 1 is", sum)
+
+	anotherBakersDozen := add(double(6), 1)
+	fmt.Println("6 squared + 1 is", anotherBakersDozen)
 }
