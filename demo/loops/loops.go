00b1bcd7514c3ac72e17ff9fab0797f8e7857059
Antonio Diaz <j.antoni.udiaz@gmail.com>
2022-05-13 11:58:42 -0500

    feat(demo): basic use for as While

--- demo/loops/loops.go
+++ demo/loops/loops.go
@@ -7,6 +7,11 @@ func main() {
 	fmt.Println("Sum of integers 1 to 10:")
 	for i := 1; i <= 10; i++ {
 		sum += i
-		fmt.Println("Sum is ", i)
+		fmt.Println("Sum is ", sum)
+	}
+
+	for sum > 10 {
+		sum -= 5
+		fmt.Println("Decrement, Sum is ", sum)
 	}
 }
