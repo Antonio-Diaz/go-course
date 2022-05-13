b950e4ade559461698f238646f3183eeb5eb13e3
Antonio Diaz <j.antoni.udiaz@gmail.com>
2022-05-12 22:48:13 -0500

    feat(demo): Basic use switch

--- demo/switch/switch.go
+++ demo/switch/switch.go
@@ -13,5 +13,23 @@ const (
 )
 
 func main() {
+	switch p := price(); {
+	case p < FirstClass:
+		fmt.Println("Cheap item")
+	case p < 10:
+		fmt.Println("Moderately priced item")
+	default:
+		fmt.Println("Expensive item")
+	}
 
+	switch ticket := Economy; {
+	case ticket == Economy:
+		fmt.Println("Economy ticket")
+	case ticket == Business:
+		fmt.Println("Business ticket")
+	case ticket == FirstClass:
+		fmt.Println("First class ticket")
+	default:
+		fmt.Println("Unknown ticket")
+	}
 }
