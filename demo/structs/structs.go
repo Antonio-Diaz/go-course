f469a2f2a936bb3c8e0e76ce5e296640611897df
Antonio Diaz <j.antoni.udiaz@gmail.com>
2022-05-15 12:27:00 -0500

    feat(demo): basic use struct

--- demo/structs/structs.go
+++ demo/structs/structs.go
@@ -2,6 +2,43 @@ package main
 
 import "fmt"
 
-func main() {
+type Passenger struct {
+	Name         string
+	TicketNumber int
+	Boarded      bool
+}
 
+type Bus struct {
+	FrontSeat Passenger
+}
+
+func main() {
+	casey := Passenger{"Casey Jones", 1, false}
+	fmt.Println(casey)
+
+	var (
+		bill = Passenger{"Bill Smith", 2, false}
+		ella = Passenger{"Ella Fitzgerald", 3, false}
+	)
+
+	fmt.Println(bill, ella)
+
+	var heidi Passenger
+	heidi.Name = "Heidi Montag"
+	heidi.TicketNumber = 4
+	fmt.Println(heidi)
+
+	casey.Boarded = true
+	bill.Boarded = true
+	if bill.Boarded {
+		fmt.Println("Bill got on the bus.")
+	}
+	if casey.Boarded {
+		fmt.Println("Casey got on the bus.")
+	}
+
+	heidi.Boarded = true
+	bus := Bus{heidi}
+	fmt.Println(bus)
+	fmt.Println(bus.FrontSeat.Name, "is in the front seat.")
 }
