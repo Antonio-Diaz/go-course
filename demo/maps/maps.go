1feb44d62339a7b47f2119ab494a0175e9527e04
Antonio Diaz <j.antoni.udiaz@gmail.com>
2022-05-15 16:44:11 -0500

    feat(demo): basic use maps

--- demo/maps/maps.go
+++ demo/maps/maps.go
@@ -3,4 +3,27 @@ package main
 import "fmt"
 
 func main() {
+	shoppingList := make(map[string]int)
+	shoppingList["eggs"] = 11
+	shoppingList["milk"] = 3
+	shoppingList["bread"] += 2
+
+	shoppingList["eggs"] += 1
+	fmt.Println(shoppingList)
+
+	delete(shoppingList, "milk")
+	fmt.Println("Milk was deleted, new list:", shoppingList)
+
+	fmt.Println("need", shoppingList["eggs"], "eggs")
+	
+	if cereal, ok := shoppingList["cereal"]; ok {
+		fmt.Println("You need", cereal, "cereal")
+	} else {
+		fmt.Println("You don't need cereal")
+	}
+
+	totalItems := 0
+	for _, amount := range shoppingList {
+		totalItems += amount
+	}
 }
