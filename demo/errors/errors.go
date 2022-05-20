8e3bc10b5b5716eb22063004124be15907061875
Antonio Diaz <j.antoni.udiaz@gmail.com>
2022-05-20 13:42:03 -0500

    feat(exercise): basic use errors

--- demo/errors/errors.go
+++ demo/errors/errors.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
-func main() {
+type Stuff struct {
+	values []int
+}
 
+func (s *Stuff) Get(index int) (int, error) {
+	if index >= len(s.values) {
+		return 0, errors.New(fmt.Sprintf("Index %v out of range", index))
+	}
+	return s.values[index], nil
+}
+
+func main() {
+	stuff := Stuff{}
+	value, err := stuff.Get(10)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(value)
+	}
 }
